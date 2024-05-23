a6b41b3c04124338360764d67629433b55110271
Andy Guajardo <andymatt1225@gmail.com>
2024-05-22 22:30:18 -0700

    edits to login, models, visual changes on FE

--- client/src/pages/Entries.jsx
+++ client/src/pages/Entries.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 // import Form from "../components/Form";
 
 
@@ -45,31 +45,20 @@ const Entries = () => {
         // console.log('params id ->', id);
     }
 
-    const newEntries = entries.map(({ _id, title, createdAt, personalContent, professionalContent }) => {
+    const newEntries = entries.map(({ _id, title}) => {
         return (
 
             <Link to={`/SingleEntry/${_id}`} key={_id}
-                // className="flex flex-col  items-start border-4 border-indigo-500 p-3"
-                className="w-full md:w-1/3 px-2 mb-4"
-            >
-                <div className="border border-indigo-500 p-3">
+                className="w-full md:w-1/3 px-2 mb-4 ">
+                    
+                    {/* <div className="border border-2 border-indigo-500 p-3 rounded-md hover:bg-gray-100 transition duration-300 ease-in-out"> */}
+                    <div className="border border-2 border-indigo-500 p-3 rounded-md 
+                    hover:bg-indigo-200 transition duration-300 ease-in-out transform hover:scale-105">
+
+
                     <ul>
-                        <li className="text-l text-blue-700">{title}</li>
-                        {/* <li>{createdAt}</li> */}
-                        {/* <li>Professional: <br />{professionalContent}</li> */}
-                        {/* <li>Personal: <br />{personalContent}</li> */}
+                        <li className="text-l text-slate-800">{title}</li>
                     </ul>
-                    {/* <button
-                        className="text-white bg-gradient-to-r from-blue-400 via-indigo-500 to-purple-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-5 mt-5 w-2/5 self-center"
-                        onClick={() => handleViewEntry(_id)}
-                    >
-                        View Entry
-                    </button> */}
-                    {/* <button
-                        className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-5 mt-5 w-2/5 self-center"
-                        onClick={() => deleteEntry(_id)}>
-                        Delete post above
-                    </button> */}
                 </div>
             </Link>
         )
@@ -87,11 +76,10 @@ const Entries = () => {
                 onClick={handleNewEntry}>Make New Entry</button>
             <br />
             <h2 className="text-4xl font-bold  text-center mb-5"> Clicky entry to view details</h2>
-            {/* Entries here: */}
-<div className="flex flex-wrap -mx-2">
+            <div className="flex flex-wrap -mx-2">
 
-            {newEntries}
-</div>
+                {newEntries}
+            </div>
             <br />
             <br />
         </div>
