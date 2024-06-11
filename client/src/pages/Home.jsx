ff221e368db4e9fa0c271a565fffd3d2a950fc54
Andy Guajardo <andymatt1225@gmail.com>
2024-06-10 23:11:03 -0700

    contact page form created, API page centered

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
   const handleClick = () => {
     navigate('/ContactPage');
   }
-  const handleClick2 = () => {
+  const handleTest2 = () => {
     navigate('/test2');
   }
   const handleAPIClick = () => {
@@ -29,57 +29,31 @@ const Home = () => {
   }
 
   return (
-    // <div className="h-screen w-screen relative">
-    //   <NavBar />
-    //   <div className="relative absolute inset-0 flex flex-col  px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] z-0">
-    //     <h2 className="z-10 text-white bg-clip-text text-6xl font-bold text-center">
-    //       Welcome to the Home Page {userName}
-    //     </h2>
-    //     <br />
-    //     <div className="z-10 flex flex-row items-center justify-center ">
-
-    //       <button
-    //         className="mt-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm h-20 px-10 py-2.5 text-center me-2 mb-1"
-    //         onClick={handleClick}>Button to Test1</button>
-    //       <br></br>
-    //       <button
-    //         className="mt-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm h-20 px-10 py-2.5 text-center me-2 mb-1 "
-    //         onClick={handleClick2}>Button to Test2</button>
-    //       <br></br>
-    //       <button
-    //         className="mt-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm h-20 px-10 py-2.5 text-center me-2 mb-1 "
-    //         onClick={handleAPIClick}>Button to API test Page </button>
-    //       <br />
-    //       <button
-    //         className="mt-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm h-20 px-10 py-2.5 text-center me-2 mb-1 "
-    //         onClick={handleViewEntries}>View Entries</button>
-    //     </div>
-    //   </div>
-    // </div>
+    
 
     <div>
       <NavBar />
-      <div className="h-screen relative absolute inset-0 flex flex-col justify-center items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] z-0">
+      <div className="h-screen relative  inset-0 flex flex-col justify-center items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] z-0">
         <h2 className="z-10 text-white bg-clip-text text-6xl font-bold text-center">
           Welcome to the Home Page {userName}
         </h2>
         <br />
         <div className="z-10 flex flex-row items-center justify-center ">
-          <button
+          {/* <button
             className="mt-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm h-20 px-10 py-2.5 text-center me-2 mb-1"
             onClick={handleClick}
           >
             Button to ContactPage
           </button>
-          <br />
+          <br /> */}
           <button
             className="mt-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm h-20 px-10 py-2.5 text-center me-2 mb-1"
-            onClick={handleClick2}
+            onClick={handleTest2}
           >
             Button to Test2
           </button>
           <br />
-          <button
+          {/* <button
             className="mt-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm h-20 px-10 py-2.5 text-center me-2 mb-1"
             onClick={handleAPIClick}
           >
@@ -91,7 +65,7 @@ const Home = () => {
             onClick={handleViewEntries}
           >
             View Entries
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
