46e544fd47f16fa0c221a817386d6201f922b2e4
Andy Guajardo <andymatt1225@gmail.com>
2024-07-07 01:16:26 -0700

    new card design, video on home page

--- client/src/pages/Entries.jsx
+++ client/src/pages/Entries.jsx
@@ -40,55 +40,73 @@ const Entries = () => {
     const handleHomeButton = () => {
         navigate('/Home');
     }
-    
-    const newEntries = entries.map(({ _id, title,createdAt }) => {
-        
-      const formattedDate = new Date(createdAt).toLocaleDateString('en-US', {
-        month: 'short',
-        day: 'numeric',
-        year: 'numeric',
-      });
+
+    const newEntries = entries.map(({ _id, title, createdAt }) => {
+
+        const formattedDate = new Date(createdAt).toLocaleDateString('en-US', {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric',
+        });
 
 
         return (
-
             <Link to={`/SingleEntry/${_id}`} key={_id}
-                className="w-full md:w-1/3 px-2 mb-4">
-                <div className="border-2 border-blue-600 p-3 rounded-md bg-white bg-opacity-70
-                    hover:bg-indigo-200 transition duration-300 ease-in-out transform hover:scale-105 h-72 overflow-hidden ">
-                    <ul>
-                        <li className="text-l text-slate-800">{formattedDate}</li>
-                        <li className="text-l text-slate-800">{title}</li>
-                        
-                    </ul>
-                    <div className="max-w-full">
-                        {mountains && <img src={mountains} alt="mountains" className="max-w-full h-auto"/>}
-                    </div>
+            className="w-full md:w-1/3 px-2 mb-4">
+
+            <div className="border-2 border-blue-600 p-3 rounded-md bg-white bg-opacity-70
+                hover:bg-indigo-200 transition duration-300 ease-in-out transform hover:scale-105 h-72 overflow-hidden ">
+                <ul>
+                    <li className="text-l text-slate-800">{title}</li>
+                </ul>
+                <div className="max-w-full">
+                    {mountains && <img src={mountains} alt="mountains" className="max-w-full h-auto" />}
+                    <p>{formattedDate}</p>
                 </div>
+            </div>
+
+
             </Link>
         )
         //.reverse to prepend entries to top, sure there's a better way but i'm tired right now
     }).reverse();
 
     return (
-        <div style={{ backgroundImage: `url(${beachIMG})`,backgroundSize: 'cover' }}>
+        <div style={{ backgroundImage: `url(${beachIMG})`, backgroundSize: 'cover' }}>
 
-    <NavBar/>
-        {/* <div className="px-6 flex flex-col items-center" > */}
-        <div className="px-6 flex flex-col items-center" >
-            <br />
-            <h1 className="text-4xl font-bold text-center mb-5"> Blog Entries</h1>
-            <h5>Click entries to view details</h5>
-            <div className="flex flex-wrap -mx-2 justify-center w-full mb-5 mt-10">
-                {newEntries}
+            <NavBar />
+
+            <div class=" w-full flex items-center justify-center ">
+        <article class="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden dark:bg-gray-600">
+          <div className="max-w-full">
+            {mountains && <img src={mountains} alt="mountains" className="max-w-full h-auto" />}
+          </div>
+          <div class="flex flex-col gap-1 mt-4 px-4">
+            <h2 class="text-lg font-semibold text-gray-800 dark:text-gray-50">Title</h2>
+            <span class="font-normal text-gray-600 dark:text-gray-300">Blog Article</span>
+            <span class="font-semibold text-gray-800 dark:text-gray-50">Data Here</span>
+          </div>
+          <div class="mt-4 p-4 border-t border-gray-200 ">
+          <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-50">Text below Line</h3>
+          </div>
+        </article>
+      </div>
+
+            {/* <div className="px-6 flex flex-col items-center" > */}
+            <div className="px-6 flex flex-col items-center" >
+                <br />
+                <h1 className="text-4xl font-bold text-center mb-5"> Blog Entries</h1>
+                <h5>Click entries to view details</h5>
+                <div className="flex flex-wrap -mx-2 justify-center w-full mb-5 mt-10">
+                    {newEntries}
+                </div>
+                <button
+                    className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-xl w-1/3 px-5 py-2.5 text-center mb-2"
+                    onClick={handleNewEntry}>
+                    Make New Entry
+                </button>
             </div>
-            <button
-                className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-xl w-1/3 px-5 py-2.5 text-center mb-2"
-                onClick={handleNewEntry}>
-                Make New Entry
-            </button>
         </div>
-                    </div>
     )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { Link, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import NavBar from '../components/navbar';
 import LogLocalStorage from '../components/LogLocalStorage';
+import stars from '../../public/videos/starryVideo.mp4'
+import mountains from '../../public/images/mountains.jpg';
 
 
 const Home = () => {
@@ -16,14 +18,14 @@ const Home = () => {
     }
   }, []);
 
-  
+
   const handleTest2 = () => {
     navigate('/test2');
   }
-  
-  
+
+
   return (
-    
+
 
     <div>
       <NavBar />
@@ -33,19 +35,44 @@ const Home = () => {
         </h2>
         <br />
         <div className="z-10 flex flex-row items-center justify-center ">
-          <button
+          {/* <button
             className="mt-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm h-20 px-10 py-2.5 text-center me-2 mb-1"
             onClick={handleTest2}
           >
             Button to Test2
-          </button>
+          </button> */}
           <br />
-          <LogLocalStorage/>
+          {/* <LogLocalStorage/> */}
+          <video className="w-full object-cover" controls autoplay>
+            <source src="/videos/starryVideo.mp4" type="video/mp4" />
+          </video>
         </div>
       </div>
+
+
+
+      <div class="flex justify-center ">
+        <article class="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden dark:bg-gray-600">
+          <div className="max-w-full">
+            {mountains && <img src={mountains} alt="mountains" className="max-w-full h-auto" />}
+
+          </div>
+
+          <div class="flex flex-col gap-1 mt-4 px-4">
+            <h2 class="text-lg font-semibold text-gray-800 dark:text-gray-50">Title</h2>
+            <span class="font-normal text-gray-600 dark:text-gray-300">Blog Article</span>
+            <span class="font-semibold text-gray-800 dark:text-gray-50">Data Here</span>
+          </div>
+
+          <div class="mt-4 p-4 border-t border-gray-200 dark:border-gray-500">
+          <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-50">Text below Line</h3>
+          </div>
+        </article>
+      </div>
+
     </div>
   );
-  
+
 }
 
 export default Home
