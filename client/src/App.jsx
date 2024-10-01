c81b6fec456596053b109a5a3c537119cef3a550
Andy Guajardo <andymatt1225@gmail.com>
2024-09-30 22:21:21 -0700

    Todo header and new page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import Signup from './pages/Signup.jsx';
 import SingleEntry from './pages/SingleEntry.jsx';
 import EditEntry from './pages/EditEntry.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
+import Todo from './pages/Todo.jsx';
 
 const App = () => {
     const navigate = useNavigate();
@@ -40,6 +41,7 @@ const App = () => {
         <Route path="/NewEntry" element={<PrivateRoute element={<NewEntry />} />} />
         <Route path="/SingleEntry/:id" element={<PrivateRoute element={<SingleEntry />} />} />
         <Route path="/EditEntry/:id" element={<PrivateRoute element={<EditEntry />} />} />
+        <Route path="/Todo" element={<PrivateRoute element={<Todo />} />} />
       </Routes>
     ) : (
       <Routes>
@@ -53,6 +55,7 @@ const App = () => {
         <Route path="/NewEntry" element={<PrivateRoute element={<NewEntry />} />} />
         <Route path="/SingleEntry/:id" element={<PrivateRoute element={<SingleEntry />} />} />
         <Route path="/EditEntry/:id" element={<PrivateRoute element={<EditEntry />} />} />
+        <Route path="/Todo" element={<PrivateRoute element={<Todo />} />} />
     </Routes>
     )}
     </div>
