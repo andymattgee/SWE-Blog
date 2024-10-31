f35059a8618dfd91b2aabcf7deb56b070027d7cd
Andy Guajardo <andymatt1225@gmail.com>
2024-10-30 22:42:09 -0700

    edit and delete in one component

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -13,6 +13,8 @@ const TodoList = ({ todo, onDeleteTodo }) => {
         }
     };
 
+    
+
     return (
         <div>
             {todo.map((todo) => (
@@ -38,18 +40,21 @@ const TodoList = ({ todo, onDeleteTodo }) => {
                     </p>
 
                     <div className="flex justify-center mt-4">
-                        <button
+                        {/* <button
                             className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2"
                             onClick={() => handleDelete(todo._id)}
                         >
                             Delete
-                        </button>
+                        </button> */}
                         <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            className="bg-yellow-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded border border-black"
                             onClick={() => navigate(`/SingleTodo/${todo._id}`)}
                         >
-                            Edit
+                            Edit | Delete
                         </button>
+
+                        
+
                     </div>
 
                 </div>
--- client/src/pages/SingleTodo.jsx
+++ client/src/pages/SingleTodo.jsx
@@ -67,7 +67,31 @@ const SingleTodo = () => {
             console.log('error from axios req ->', error);
         }
         
+    };
+
+    const deleteTodo = async (e,id) => {
+        e.preventDefault();
+        const confirmation = window.confirm("Are you sure you want to delete this todo?");
+        if (confirmation) {
+            try {   
+                console.log('enter deleteTodo block');
+                const token = localStorage.getItem('token');
+                const response = await axios.delete(`http://localhost:3333/api/todos/${id}`, {
+                    headers: {
+                        Accept: 'application/json',
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                console.log('response.data ->', response.data.data);
+                navigate('/todos'); // only navigate if deletion is confirmed
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            // do nothing if user cancels
+        }
     }
+
 
     const renderTodo = () => {
         if (!todo) {
@@ -104,7 +128,7 @@ const SingleTodo = () => {
             <NavBar />
             {/* {renderTodo()} */}
             <div>
-                <h3> Edit to go here</h3>
+                <h3> Edit to do here</h3>
 
                 <form onSubmit={handleSubmit} className="w-8/12">
                     <div className="mb-4 ">
@@ -190,6 +214,12 @@ const SingleTodo = () => {
                             className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 h-20 w-full"
                         >
                             Save Changes
+                        </button>
+                        <button
+                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2"
+                            onClick={(e) => deleteTodo(e,id)}
+                        >
+                            Delete ToDo
                         </button>
                     </div>
 
